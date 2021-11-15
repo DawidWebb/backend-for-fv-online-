6ecf735ac01bf58338eba5dc118a6cc63bfe6367
Tomasz Matras <tomasz.matras@developerweb.pl>
2021-11-15 21:05:07 +0100

    Delete _config-omega.js

--- config/_config-omega.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: "mongodb+srv://omegazlecenia:d4TfKY1uLii0bqWz@cluster0.4b0bi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  keySession: ["server-form-session"],
-  maxAgeSession: 1 * 60 * 60 * 1000, // 1h
-};
