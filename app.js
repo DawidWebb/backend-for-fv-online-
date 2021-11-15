b2dd1426f328f599d9b1b275c457a4f74f16fc8c
DawidWebb <tomasz.matras@developerweb.pl>
2021-11-15 20:15:43 +0100

    add DB for omega

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const config = require("./_config");
+const config = require("./config/_configOmega.js");
 const mongoose = require("mongoose");
 
 //* mongoose conect *//
---
+++ config/_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db:
+    "mongodb+srv://admin:4lPUK77vA6JHVHPu@cluster0.uqf78.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  keySession: ["server-form-session"],
+  maxAgeSession: 1 * 60 * 60 * 1000, // 1h
+};
